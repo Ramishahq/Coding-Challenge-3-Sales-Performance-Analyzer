47bee55e316671718b548d1a523e99ea0f398cb3
ramishahq <hoque2@usf.edu>
2024-09-07 00:37:25 -0400

    Develop findTopAndBottomPerformers function

--- Sales Performance Analyzer.js
+++ Sales Performance Analyzer.js
@@ -8,15 +8,36 @@ function calculateAverageSales (sales){
 
 // Task 2: Create a Function to Determine Performance Rating
 
-funtion determinePerformanceRating (averageSales) {
+function determinePerformanceRating (averageSales) {
 if (averageSales > 10000) {
-    return"Excelent";
+    return"Excellent";
 } else if (averageSales >= 7000 && averageSales <= 10000){
         return "Good";
 }else if (averageSales >= 4000 && averageSales < 7000) {
     return "Satisfactory";
-}else if (averageSales < 4000) {
+}else if {
     return "Needs Improvement";
   }
 }
 
+// Task 3: Create a Function to Identify Top and Bottom Performers
+// i am using Array.prototype.reduce()
+
+function findTopAndBottomPerformers(salesPeople) {
+    const result = salesPeople.reduce((result, salesPerson) => {
+        
+        if (salesPerson.totalSales > result.topPerformer.totalSales) {
+          result.topPerformer = salesPerson;
+        }
+    // above I compared current salesPerson with the top performer
+    if (salesPerson.totalSales < result.bottomPerformer.totalSales) {
+        result.bottomPerformer = salesPerson;
+      }
+      // above I compared current salesPerson with the bottom performer
+      return result;
+}, {
+    topPerformer: salesPeople[0], 
+    bottomPerformer: salesPeople[0] 
+});
+return result;
+}
